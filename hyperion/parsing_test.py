c5235c880e05e0ba89c737e3550e8a7cfb512060
Piotr Kozakowski <kozak000@gmail.com>
2020-12-20 21:32:10 +0100

    Turn down the settings of parsing tests.

--- hyperion/parsing_test.py
+++ hyperion/parsing_test.py
@@ -1,4 +1,4 @@
-import hypothesis
+import hypothesis as ht
 from hypothesis import strategies as st
 
 from hyperion import parsing
@@ -6,22 +6,29 @@ from hyperion import rendering
 from hyperion import testing
 
 
-max_examples = 20
+settings = {
+    # Disable the shrinking phase - it's really slow for parsing.
+    "phases": (ht.Phase.explicit, ht.Phase.reuse, ht.Phase.generate, ht.Phase.target),
+    # Extend the deadline to fit more complex trees.
+    "deadline": 500,
+    # Lower the number of examples to match.
+    "max_examples": 50,
+}
 
 
-@hypothesis.settings(max_examples=max_examples)
-@hypothesis.given(testing.configs())
+@ht.settings(**settings)
+@ht.given(testing.configs())
 def test_parse_config_inverses_render(original_config):
     text = rendering.render(original_config)
-    hypothesis.note(f"Rendered config: {text}")
+    ht.note(f"Rendered config: {text}")
     parsed_config = parsing.parse_config(text)
     assert parsed_config == original_config
 
 
-@hypothesis.settings(max_examples=max_examples)
-@hypothesis.given(testing.sweeps())
+@ht.settings(**settings)
+@ht.given(testing.sweeps())
 def test_parse_sweep_inverses_render(original_sweep):
     text = rendering.render(original_sweep)
-    hypothesis.note(f"Rendered sweep: {text}")
+    ht.note(f"Rendered sweep: {text}")
     parsed_sweep = parsing.parse_sweep(text)
     assert parsed_sweep == original_sweep
